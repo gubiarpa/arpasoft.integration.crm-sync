6f8c838155645d70d483de037f57669de13e62a9
gubiarpa <billy.arredondo@outlook.com>
2019-08-09 17:32:14 -0500

    Se contrajo las definiciones de las clases tipo ID-Descripción.

--- Expertia.Estructura/Expertia.Estructura/Models/CuentaForeignKey.cs
+++ Expertia.Estructura/Expertia.Estructura/Models/CuentaForeignKey.cs
@@ -9,72 +9,40 @@
     #endregion
 
     #region Cuenta
-    public class TipoPersona : SimpleDesc
-    {
-    }
+    public class TipoPersona : SimpleDesc { }
 
-    public class PuntoContacto : SimpleDesc
-    {
-    }
+    public class PuntoContacto : SimpleDesc { }
 
-    public class NivelImportancia : SimpleDesc
-    {
-    }
+    public class NivelImportancia : SimpleDesc { }
 
-    public class TipoCuenta : SimpleDesc
-    {
-    }
+    public class TipoCuenta : SimpleDesc { }
 
-    public class Estado : SimpleDesc
-    {
-    }
+    public class Estado : SimpleDesc { }
 
-    public class PaisProcedencia : SimpleDesc
-    {
-    }
+    public class PaisProcedencia : SimpleDesc { }
     #endregion
 
     #region CuentaB2B
-    public class CondicionPago : SimpleDesc
-    {
-    }
+    public class CondicionPago : SimpleDesc { }
 
-    public class TipoMoneda : SimpleDesc
-    {
-    }
+    public class TipoMoneda : SimpleDesc { }
     #endregion
 
     #region CuentaB2C
-    public class EstadoCivil : SimpleDesc
-    {
-    }
+    public class EstadoCivil : SimpleDesc { }
 
-    public class Genero : SimpleDesc
-    {
-    }
+    public class Genero : SimpleDesc { }
 
-    public class Nacionalidad : SimpleDesc
-    {
-    }
+    public class Nacionalidad : SimpleDesc { }
 
-    public class GradoEstudios : SimpleDesc
-    {
-    }
+    public class GradoEstudios : SimpleDesc { }
 
-    public class Profesion : SimpleDesc
-    {
-    }
+    public class Profesion : SimpleDesc { }
 
-    public class TipoViaje : SimpleDesc
-    {
-    }
+    public class TipoViaje : SimpleDesc { }
 
-    public class CategoriaViaje : SimpleDesc
-    {
-    }
+    public class CategoriaViaje : SimpleDesc { }
 
-    public class TipoAcompanante : SimpleDesc
-    {
-    }
+    public class TipoAcompanante : SimpleDesc { }
     #endregion
 }
