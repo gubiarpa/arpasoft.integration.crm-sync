28643568f3a4a846441eb51f366345afd3f80a8c
gubiarpa <billy.arredondo@outlook.com>
2019-08-21 18:30:36 -0500

    Se agregó preliminarmente la entidad Pago.

---
+++ Expertia.Estructura/Expertia.Estructura/Models/Pago.cs
@@ -0,0 +1,21 @@
+﻿using Expertia.Estructura.Models.Auxiliar;
+using Expertia.Estructura.Models.Behavior;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Expertia.Estructura.Models
+{
+    public class Pago : IUnique, ISalesForce, IAuditable
+    {
+        #region Properties
+        public string ID { get; set; }
+        public string IdSalesForce { get; set; }
+        #endregion
+
+        #region Auditoria
+        public Auditoria Auditoria { get; set; }
+        #endregion
+    }
+}
