8c7c7fefcb7cdf36d7f9367556d1d5ad35f86841
Billy Arredondo <billy.arredondo@pucp.pe>
2019-09-13 16:50:17 -0500

    Se remueven elementos MDM
    La API estaba pensada para interactuar con el MDM, pero en el avance, el proyecto, se prescinde de éste.

--- Expertia.Estructura/Expertia.Estructura/Repository/Behavior/ILookupId.cs
+++
@@ -1,9 +0,0 @@
-﻿using Expertia.Estructura.Repository.MDM;
-
-namespace Expertia.Estructura.Repository.Behavior
-{
-    public interface ILookupId
-    {
-        object LookUpByDescription(CuentaB2B_FK fieldName, object description = null);
-    }
-}
