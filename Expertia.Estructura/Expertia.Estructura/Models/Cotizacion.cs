bf1be73b7de6f13876ede7ac66ce5f0aeedd2095
gubiarpa <billy.arredondo@pucp.pe>
2020-02-20 10:47:46 -0500

    [Journeyou] Cotizacion Added
    Se agregó el controlador de Cotización para Journeyou.

--- Expertia.Estructura/Expertia.Estructura/Models/Cotizacion.cs
+++ Expertia.Estructura/Expertia.Estructura/Models/Cotizacion.cs
@@ -95,4 +95,34 @@ namespace Expertia.Estructura.Models
         public string CodigoError { get; set; }
         public string MensajeError { get; set; }
     }
+
+    public class Cotizacion_JY
+    {
+        public string IdOportunidad_sf { get; set; }
+        public string IdCotizacionSf { get; set; }
+        public string IdCuentaSf { get; set; }
+        public string Usuario { get; set; }
+        public string Accion { get; set; }
+        public string TipoDocumento { get; set; }
+        public string Documento { get; set; }
+        public string Correo { get; set; }
+        public string Region { get; set; }
+        public string Cotizacion { get; set; }
+        public int NumeroPaxs { get; set; }
+    }
+
+    public class CotizacionResponse_JY : ICrmApiResponse
+    {
+        public string CodigoError { get; set; }
+        public string MensajeError { get; set; }
+        public string Grupo { get; set; }
+        public string Estado { get; set; }
+        public float? VentaEstimada { get; set; }
+        public string FileSubfile { get; set; }
+        public float? VentaFile { get; set; }
+        public float? MargenFile { get; set; }
+        public int? PaxsFile { get; set; }
+        public string EstadoFile { get; set; }
+
+    }
 }
