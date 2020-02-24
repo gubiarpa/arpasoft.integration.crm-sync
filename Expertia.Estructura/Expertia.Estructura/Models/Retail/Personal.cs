8dd081df8ccfde644b1da3d40745914b682f1d20
gubiarpa <billy.arredondo@pucp.pe>
2020-02-24 18:07:49 -0500

    [Retail] Modelo Personal Agregado
    Se agregó el modelo Personal dentro de la carpeta Models/Reatail.

---
+++ Expertia.Estructura/Expertia.Estructura/Models/Retail/Personal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Expertia.Estructura.Models.Retail
+{
+    public class Personal
+    {
+        public DateTime? FecUpdate { get; set; }
+        public bool EsCounterAdmin { get; set; }
+        public bool TieneRpteMetas { get; set; }
+        public int IdUsuWeb { get; set; }
+        public string LoginPer { get; set; }
+        public string ApePatPer { get; set; }
+        public string ApeMatPer { get; set; }
+        public string NomPer { get; set; }
+        public string DirecPer { get; set; }
+        public string CodPostalPer { get; set; }
+        public string EmailPer { get; set; }
+        public string TelfCasaPer { get; set; }
+        public string TelfMovilPer { get; set; }
+        public string TelfMovilEmpresaPer { get; set; }
+        public string TelfDirectoPer { get; set; }
+        public string TelfOficinaPer { get; set; }
+        public string TelfNextelPer { get; set; }
+        public string TelfEmergenciaPer { get; set; }
+        public string AnexoPer { get; set; }
+        public string NumDocPer { get; set; }
+        public string TipoSangrePer { get; set; }
+        public string FechaNacPer { get; set; }
+        public int EstadoPer { get; set; }
+        public int JefeInmediatoPer { get; set; }
+        public string NumAFPPer { get; set; }
+        public string NumSeguroPer { get; set; }
+        public string FecIngresoPer { get; set; }
+        public float SueldoBrutoPer { get; set; }
+        public float SueldoNetoPer { get; set; }
+        public string EnfermedadesPer { get; set; }
+        public string DeportesPer { get; set; }
+        public string HobbiesPer { get; set; } 
+        public string RutaCVPer { get; set; }
+        public string IdDoc { get; set; }
+        public string IdPais { get; set; }
+        public string IdCiudad { get; set; }
+        public int IdDistrito { get; set; }
+        public int IdDepartamento { get; set; }
+        public int IdOficina { get; set; }
+        public int IdEmpresa { get; set; }
+        public int IdTipoDomicilio { get; set; }
+        public int IdDomicilio { get; set; }
+        public int IdEmpresaPension { get; set; }
+        public int IdGdoInstruccion { get; set; }
+        public int IdEstadoCivil { get; set; }
+        public int IdMonedaSBruto { get; set; }
+        public int IdMonedaSNeto { get; set; }
+        public string NomCompletoPer  { get; set; }
+        public int IdUsuWebSybase { get; set; }
+        public bool EsSupervisorSRV { get; set; }
+        public int IdCargo { get; set; }
+        public bool VarDespegar { get; set; }
+        public bool VarImgDespegar { get; set; }
+        public string PCCPer { get; set; }
+        public int? IdPlanilla { get; set; }
+        public int? IdEmpresaPlanilla { get; set; }
+        public string AutoLoginTrp { get; set; }
+    }
+}
