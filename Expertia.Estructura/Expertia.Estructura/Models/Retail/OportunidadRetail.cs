659d3ef29bd9ff5e69d2bb0bac8acb27e6342078
gubiarpa <billy.arredondo@pucp.pe>
2020-02-24 19:34:18 -0500

    [Retail] OportunidadReatail reubicado
    Se reubicó a la clase tipo Model OportunidadRetail dentro de la carpeta Model/Retail para un mejor orden del proyecto.

---
+++ Expertia.Estructura/Expertia.Estructura/Models/Retail/OportunidadRetail.cs
@@ -0,0 +1,39 @@
+﻿using Expertia.Estructura.Models.Behavior;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Expertia.Estructura.Models.Retail
+{
+    public class OportunidadRetailReq
+    {
+        public string Numdoc { get; set; }
+        public string IdTipoDoc { get; set; }
+        public string IdCanalVenta { get; set; }
+        public string NombreCli { get; set; }
+        public string ApePatCli { get; set; }
+        public string ApeMatCli { get; set; }
+        public string EmailCli { get; set; }
+        public string IdDestino { get; set; }
+        public string EnviarPromociones { get; set; }
+        public int MotivoCrea { get; set; }
+        public string Area { get; set; }
+        public int UsuarioCrea { get; set; }
+        public string Comentario { get; set; }
+        public int IdCotSRV { get; set; }
+        public int IdOportunidad_SF { get; set; }
+        public int IdUsuarioSrv_SF { get; set; }
+        public string Accion_SF { get; set; }
+    }
+
+    public class OportunidadRetailRes : ICrmApiResponse
+    {
+        public string CodigoError { get; set; }
+        public string MensajeError { get; set; }
+        public int IdOportunidad_SF { get; set; }
+        public int IdCotSrv { get; set; }
+        public string FechaCreacion { get; set; }
+
+    }
+}
