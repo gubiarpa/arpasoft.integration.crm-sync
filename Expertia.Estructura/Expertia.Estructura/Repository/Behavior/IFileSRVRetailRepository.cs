d2a962782696403cce18e5dfed886e46e2ffc6a6
Ysrael Núñez Miranda <50840775+YsraelGNM@users.noreply.github.com>
2020-03-09 18:12:13 -0500

    [Retail] Actualizacion File SRV

--- Expertia.Estructura/Expertia.Estructura/Repository/Behavior/IFileSRVRetailRepository.cs
+++ Expertia.Estructura/Expertia.Estructura/Repository/Behavior/IFileSRVRetailRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Expertia.Estructura.Models;
+using Expertia.Estructura.Models.Retail;
 using Expertia.Estructura.Utils;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ namespace Expertia.Estructura.Repository.Behavior
     public interface IFileSRVRetailRepository
     {
         Operation GetFilesAsociadosSRV();
-        
+        Operation Actualizar_EnvioCotRetail(FilesAsociadosSRVResponse FileAsociadosSRVResponse);
+
     }
 }
--- Expertia.Estructura/Expertia.Estructura/Repository/Retail/FileSRVRetailRepository.cs
+++ Expertia.Estructura/Expertia.Estructura/Repository/Retail/FileSRVRetailRepository.cs
@@ -26,12 +26,19 @@ namespace Expertia.Estructura.Repository.Retail
             var operation = new Operation();
             try
             {
-                #region Parameters                
+                #region Parameters
+                /// (01) P_CODIGO_ERROR
+                AddParameter(OutParameter.CodigoError, OracleDbType.Varchar2, DBNull.Value, ParameterDirection.Output, OutParameter.DefaultSize);
+                /// (02) P_MENSAJE_ERROR
+                AddParameter(OutParameter.MensajeError, OracleDbType.Varchar2, DBNull.Value, ParameterDirection.Output, OutParameter.DefaultSize);
+                /// (03) P_FILES_ASOCIADOS_SRV
                 AddParameter(OutParameter.CursorFilesAsociadosSRV, OracleDbType.RefCursor, DBNull.Value, ParameterDirection.Output, OutParameter.DefaultSize);
                 #endregion
 
                 #region Invoke              
                 ExecuteStoredProcedure(StoredProcedureName.AW_Read_FileAsociadosSRV);
+                operation[OutParameter.CodigoError] = GetOutParameter(OutParameter.CodigoError);
+                operation[OutParameter.MensajeError] = GetOutParameter(OutParameter.MensajeError);
                 operation[OutParameter.CursorFilesAsociadosSRV] = FillFilesAsociadosSRV(GetDtParameter(OutParameter.CursorFilesAsociadosSRV));
                 #endregion
             }
@@ -41,10 +48,40 @@ namespace Expertia.Estructura.Repository.Retail
             }
             return operation;
         }
+
+        public Operation Actualizar_EnvioCotRetail(FilesAsociadosSRVResponse FileAsociadosSRVResponse)
+        {
+            var operation = new Operation();
+
+            #region Parameter
+            /// (01) P_CODIGO_ERROR
+            AddParameter(OutParameter.CodigoError, OracleDbType.Varchar2, DBNull.Value, ParameterDirection.Output, OutParameter.DefaultSize);
+            /// (02) P_MENSAJE_ERROR
+            AddParameter(OutParameter.MensajeError, OracleDbType.Varchar2, DBNull.Value, ParameterDirection.Output, OutParameter.DefaultSize);
+            /// (03) P_OPORTUNIDAD_CRM
+            AddParameter("P_OPORTUNIDAD_CRM", OracleDbType.Varchar2, FileAsociadosSRVResponse.id_oportunidad_sf);
+            /// (04) P_ES_ATENCION
+            AddParameter("P_ES_ATENCION", OracleDbType.Varchar2, FileAsociadosSRVResponse.codigo_error);
+            /// (05) P_DESCRIPCION
+            AddParameter("P_DESCRIPCION", OracleDbType.Varchar2, FileAsociadosSRVResponse.mensaje_error);
+            /// (06) P_ACTUALIZADOS
+            AddParameter(OutParameter.NumeroActualizados, OracleDbType.Varchar2, DBNull.Value, ParameterDirection.Output, OutParameter.DefaultSize);
+            #endregion
+
+            #region Invoke
+            ExecuteStoredProcedure(StoredProcedureName.AW_Upd_EnvioCotRetail);
+            operation[OutParameter.CodigoError] = GetOutParameter(OutParameter.CodigoError);
+            operation[OutParameter.MensajeError] = GetOutParameter(OutParameter.MensajeError);
+            operation[OutParameter.NumeroActualizados] = GetOutParameter(OutParameter.NumeroActualizados);
+            #endregion
+
+            return operation;
+        }
+
         #endregion
 
         #region Auxiliares
-  
+
         private IEnumerable<FilesAsociadosSRV> FillFilesAsociadosSRV(DataTable dt)
         {
             try
